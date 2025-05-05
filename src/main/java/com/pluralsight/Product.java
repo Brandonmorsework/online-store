53e95b7ca4482a5008708dcf033b76b7e71a074e
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-05 09:11:07 -0400

    Added Store main class and Product class along with constructor, getters and setters

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
 public class Product {
+
+    private String iD;
+    private String productName;
+    private double price;
+
+    public Product(String iD, String productName, double price) {
+        this.iD = iD;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getiD() {
+        return iD;
+    }
+
+    public void setiD(String iD) {
+        this.iD = iD;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/com/pluralsight/Store.java
+++ src/main/java/com/pluralsight/Store.java
@@ -1,4 +1,46 @@
 package com.pluralsight;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Store {
+
+    private static ArrayList<Product> products = new ArrayList<>();
+    private static final String FILE_NAME = "Products.csv";
+
+    public static void main(String[] args) {
+        loadProducts(FILE_NAME);
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+    }
+
+
+
+
+    public static void loadProducts(String fileName) {
+
+        String line;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                String iD = parts[0];
+                String productName = parts[1];
+                double price = Double.parseDouble(parts[2]);
+
+                Product product = new Product(iD, productName, price);
+                products.add(product);
+
+            }
+
+            br.close();
+
+        } catch (Exception e) {
+            System.out.println("Error, File Not Found!");
+        }
+    }
 }
